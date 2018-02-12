4c2f85ba35acb98d171090422cccd50024bd8924
muffinman148 <pandamcg@gmail.com>
2018-02-11 21:59:18 -0800

    Initial commit for everything.

---
+++ Assets/_Scripts/ExtraLife.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExtraLife : MonoBehaviour {
+
+//	void OnCollisionEnter(Collision Other)
+//	{
+//		GM.instance.GainLife ();
+//	}
+}
---
+++ Assets/_Scripts/Paddle.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour {
+
+	public float paddleSpeed = 1f;
+
+	private Vector3 PlayerPos = new Vector3(0, -9.5f, 0);
+
+
+	// Update is called once per frame
+	void Update () 
+	{
+		float xPos = transform.position.x + (Input.GetAxis ("Horizontal") * paddleSpeed);
+		PlayerPos = new Vector3 (Mathf.Clamp (xPos, -7f, 7f), -9.5f, 0f);
+		transform.position = PlayerPos;
+	}
+}
